0856dc66aee0fb8a4d11c297f4cbf6b69b47a659
krunal <krunalmiracle@gmail.com>
2021-11-26 15:15:49 +0100

    Minor change: Voice Rate TTS Offline

--- demo_tts.py
+++ demo_tts.py
@@ -8,7 +8,7 @@ engine = pyttsx3.init()
 """ RATE"""
 rate = engine.getProperty('rate')   # getting details of current speaking rate
 print (rate)                        #printing current voice rate
-engine.setProperty('rate', 125)     # setting up new voice rate
+engine.setProperty('rate', 165)     # setting up new voice rate
 
 
 """VOLUME"""
